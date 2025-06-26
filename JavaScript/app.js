50f91f44b12d35c35e8f81a0bf98af25bb7dd961
CarnageOG <egetashviligiorgi9@gmail.com>
2025-06-26 19:51:17 +0400

    made layout

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,5 +1,20 @@
+// საიდბარის ჩამშლა
 function toggleDropdown(event) {
   event.preventDefault();
   const container = event.currentTarget.parentElement;
   container.classList.toggle('open');
 }
+
+// უზერ მოდალი
+const userBtn = document.querySelectorAll('.icon-btn')[1];
+  const userModal = document.getElementById('user-modal');
+
+  userBtn.addEventListener('click', () => {
+    userModal.classList.toggle('hidden');
+  });
+
+  document.addEventListener('click', (event) => {
+    if (!userBtn.contains(event.target) && !userModal.contains(event.target)) {
+      userModal.classList.add('hidden');
+    }
+  });
