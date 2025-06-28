d794b1d7ce40507151688462bc17a5c96eaefc48
CarnageOG <egetashviligiorgi9@gmail.com>
2025-06-28 18:33:36 +0400

    added payment method page

--- JavaScript/method.js
+++ JavaScript/method.js
@@ -40,3 +40,22 @@ const form = document.querySelector(".payment-form");
       document.getElementById("cardModal").style.display = "none";
     }, 2000);
   });
+
+  // ღილაკები
+
+  document.querySelectorAll('.card-btn').forEach(button => {
+    const img = button.querySelector('img');
+    if (!img) return;
+
+    if (img.alt === 'Star') {
+      button.addEventListener('click', () => {
+        button.classList.toggle('star-active');
+      });
+    }
+
+    if (img.alt === 'Delete') {
+      button.addEventListener('click', () => {
+        button.classList.toggle('delete-active');
+      });
+    }
+  });
